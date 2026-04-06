3f86c1a041d5d9d959c7224c40b624f6a9ce6b60
Tamorabrown12 <tbrow118@aum.edu>
2026-04-06 10:57:18 -0500

    Added "endl;"

--- Tamora_Lab1/Tamora_Lab1.cpp
+++ Tamora_Lab1/Tamora_Lab1.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello from Tamora Brown! This is my first Git commit."; 
+    cout << "Hello from Tamora Brown! This is my first Git commit." << endl;
     cout << "Git makes collaboration easy!" << endl; 
 }
