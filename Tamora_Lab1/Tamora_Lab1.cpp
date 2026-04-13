9743b643de0db36fbe2edc24d873abb00a078665
Tamorabrown12 <tbrow118@aum.edu>
2026-04-13 10:54:26 -0500

    Changed "Student A" to "Tamora"

--- Tamora_Lab1/Tamora_Lab1.cpp
+++ Tamora_Lab1/Tamora_Lab1.cpp
@@ -8,5 +8,5 @@ int main()
     cout << "Hello from Tamora Brown! This is my first Git commit." << endl;
     cout << "Git makes collaboration easy!" << endl; 
 
-    cout << "Feature by [Your Name]: Branching is awesome!" << endl;
+    cout << "Feature by Tamora: Branching is awesome!" << endl;
 }
